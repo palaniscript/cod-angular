b5e006d09d8af0bae996e1f18a5ce2e99da0232e
Palanisamy Thangamuthu <palaniscript@gmail.com>
2022-07-16 22:12:16 +0530

    updated login page UI

--- code-of-duty/src/app/login/login.component.ts
+++ code-of-duty/src/app/login/login.component.ts
@@ -1,52 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { first } from 'rxjs/operators';
-
-import { LoginService } from '../shared/login.service';
 import { AlertService } from '../shared/alert.service';
+import { LoginService } from '../shared/login.service';
 
-@Component({ templateUrl: 'login.component.html' })
-export class LoginComponent implements OnInit { 
-    loginForm: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private loginService: LoginService,
-        private alertService: AlertService,
-        
-    ) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private loginService: LoginService,
+    private alertService: AlertService
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  getFormControl(field: string) {
+    return this.loginForm.get(field);
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.alertService.clear();
+    if (this.loginForm.invalid) {
+      return;
     }
 
-    ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
-    }
-    get f() { return this.loginForm.controls; }
-    onSubmit() {
-        this.submitted = true;
-        this.alertService.clear();
-        if (this.loginForm.invalid) {
-            return;
+    this.loading = true;
+    this.loginService
+      .login(this.loginForm.value.username, this.loginForm.value.password)
+      .pipe(first())
+      .subscribe(
+        (data) => {
+          this.router.navigate(['dashboard']);
+        },
+        (error) => {
+          this.alertService.error(error.error.message);
+          this.loading = false;
         }
-
-        this.loading = true;
-        this.loginService.login(this.loginForm.value.username, this.loginForm.value.password)
-            .pipe(first())
-            .subscribe(
-                data => {
-                    this.router.navigate(['dashboard']);
-                },
-                error => {
-                    this.alertService.error(error.error.message);
-                    this.loading = false;
-                });
-    }
+      );
+  }
 }
