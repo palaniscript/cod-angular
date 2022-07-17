ad7d2cb4efffe74dd96b7ba0e5125e75f39a9229
Palanisamy Thangamuthu <palaniscript@gmail.com>
2022-07-17 18:41:35 +0530

    removed unused modules

--- code-of-duty/src/app/app-material/app-material.module.ts
+++ code-of-duty/src/app/app-material/app-material.module.ts
@@ -1,26 +1,19 @@
 import { NgModule } from "@angular/core";
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from "@angular/material/button";
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from "@angular/material/card";
-import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from "@angular/material/list";
 import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from "@angular/material/select";
-import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from "@angular/material/tabs";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTooltipModule } from "@angular/material/tooltip";
 
@@ -30,11 +23,8 @@ import { MatTooltipModule } from "@angular/material/tooltip";
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
-    MatChipsModule,
     MatCardModule,
-    MatSidenavModule,
     MatListModule,
-    MatTabsModule,
     MatTooltipModule,
     MatDialogModule,
     MatTableModule,
@@ -43,12 +33,8 @@ import { MatTooltipModule } from "@angular/material/tooltip";
     MatFormFieldModule,
     MatInputModule,
     MatSortModule,
-    MatProgressBarModule,
     MatSnackBarModule,
     MatDividerModule,
-    MatExpansionModule,
-    MatButtonToggleModule,
-    MatProgressSpinnerModule,
     MatFormFieldModule,
     MatInputModule,
     MatAutocompleteModule
